6e6aeb1bf5a100d7930156ba3bf86163dc786862
Kholkin <kholkins@mail.ru>
2020-02-16 14:19:22 +0300

    Add intent in last activity to back in main activity

--- app/src/main/java/com/example/andro/LastActivity.java
+++ app/src/main/java/com/example/andro/LastActivity.java
@@ -2,7 +2,9 @@ package com.example.andro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LastActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class LastActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_last);
+    }
+
+    public void onBack (View view) {
+        Intent intent = new Intent(this, MainActivity.class);
     }
 }
