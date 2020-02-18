1701c37f8e80cd0a188290e0b12634ab1de2b3cd
Kholkin <kholkins@mail.ru>
2020-02-18 20:56:34 +0300

    Add intent in last activity to back in main activity

--- app/src/main/java/com/example/andro/MainActivity.java
+++ app/src/main/java/com/example/andro/MainActivity.java
@@ -33,4 +33,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, LastActivity.class);
         startActivity(intent);
     }
+
+    public void sentData (View view) {
+        Intent intent = new Intent(this, LastActivity.class);
+        startActivity(intent);
+    }
 }
